23d62ccf3a43d4c6471ac4fe3b2ed24fa4745448
Hiren Bharodiya <hiren.bharodia@gmail.com>
2024-09-12 15:56:54 +0530

    update podspec

---
+++ DerivedData/UInterfaceIOS/Index.noindex/Build/Products/Debug-iphonesimulator/uInterfaceSDK.framework/Headers/uInterfaceSDK.h
@@ -0,0 +1,16 @@
+//
+//  uInterfaceSDK.h
+//  uInterfaceSDK
+//
+//  Created by user on 06/05/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for uInterfaceSDK.
+FOUNDATION_EXPORT double uInterfaceSDKVersionNumber;
+
+//! Project version string for uInterfaceSDK.
+FOUNDATION_EXPORT const unsigned char uInterfaceSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <uInterfaceSDK/PublicHeader.h>
